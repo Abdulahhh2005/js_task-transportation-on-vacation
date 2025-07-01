5d62c0e7dbfa20b2230ac7428fae84e1748eb12a
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-07-01 10:56:47 +0300

    Solve task calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let price = days * 40;
+
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
+
+  const longTerm = 7;
+  const longTermDiscount = 50;
+
+  if (days >= longTerm) {
+    return (price -= longTermDiscount);
+  }
+
+  if (days >= shortTerm) {
+    return (price -= shortTermDiscount);
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
